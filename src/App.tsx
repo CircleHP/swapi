99bf3a2a4a665d6712cc85fba8f0b1258d3ec47e
CircleHP <evmenovdv@gmail.com>
2020-09-04 13:26:41 +0300

    Add font and global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import 'assets/global.css';
+
 import Ships from 'screens/Ships';
 import ShipDetailed from 'screens/ShipDetailed';
 
