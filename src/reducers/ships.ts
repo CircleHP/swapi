3ffe46d88f445b0bbeef1376d648c2fd51182a5a
CircleHP <evmenovdv@gmail.com>
2020-09-04 13:36:41 +0300

    Fix reducer and selectors

--- src/reducers/ships.ts
+++ src/reducers/ships.ts
@@ -1,39 +1,40 @@
 export interface TShips {
-    data: {
-        count: number,
-        next: string,
-        previous: string,
-        results: Array<{
-            MGLT: string,
-            cargo_capacity: string,
-            consumables: string,
-            cost_in_credits: string,
-            created: string,
-            crew: string,
-            edited: string,
-            films: Array<string>,
-            hyperdrive_rating: string,
-            length: string,
-            manufacturer: string,
-            max_atmosphering_speed: string,
-            model: string,
-            name: string,
-            passengers: string,
-            pilots: Array<string>,
-            starship_class: string,
-            url: string,
-        }>
-    } | null;
+    count: number | null,
+    next: string | null,
+    previous: string | null,
+    results: Array<{
+        MGLT: string,
+        cargo_capacity: string,
+        consumables: string,
+        cost_in_credits: string,
+        created: string,
+        crew: string,
+        edited: string,
+        films: Array<string>,
+        hyperdrive_rating: string,
+        length: string,
+        manufacturer: string,
+        max_atmosphering_speed: string,
+        model: string,
+        name: string,
+        passengers: string,
+        pilots: Array<string>,
+        starship_class: string,
+        url: string,
+    }>
 };
 
 const initialState = {
-    data: null,
-};
+    count: null,
+    next: null,
+    previous: null,
+    results: [],
+}
 
 export default (state: TShips = initialState, action: any) => {
     switch (action.type) {
         case 'FETCH_SHIPS_SUCCES':
-            return state.data = action.payload;
+            return state = action.payload;
 
         default:
             return state;
--- src/selectors/index.ts
+++ src/selectors/index.ts
@@ -1,11 +1,11 @@
 import { TReduxState } from 'reducers';
 
 // ships data
-export const getShips = (state: TReduxState) => state.ships.data?.results;
+export const getShips = (state: TReduxState) => state.ships?.results;
 
 // counter
-export const getCount = (state: TReduxState) => state.ships.data?.count;
+export const getCount = (state: TReduxState) => state.ships?.count;
 
 // pagination
-export const getNextPageURL = (state: TReduxState) => state.ships.data?.next;
-export const getPreviosPageURL = (state: TReduxState) => state.ships.data?.previous;
+export const getNextPageURL = (state: TReduxState) => state.ships?.next;
+export const getPreviosPageURL = (state: TReduxState) => state.ships?.previous;
